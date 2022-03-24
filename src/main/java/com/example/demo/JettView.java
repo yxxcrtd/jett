c2fa44319536d07b86b4c31ef3653fdb369951c2
yxx <272795968@qq.com>
2022-03-24 14:55:07 +0800

    no message

---
+++ src/main/java/com/example/demo/JettView.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.web.servlet.view.AbstractView;
+
+import net.sf.jett.transform.ExcelTransformer;
+
+public class JettView extends AbstractView {
+    private String templatePath;
+    private String exportFileName;
+
+    public JettView(String templatePath, String exportFileName) {
+        this.templatePath = templatePath;
+        if (exportFileName != null) {
+            try {
+                exportFileName = URLEncoder.encode(exportFileName, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        this.exportFileName = exportFileName;
+        setContentType("application/vnd.ms-excel");
+    }
+
+    @Override
+    protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream(templatePath)) {
+            response.setContentType(getContentType());
+            response.setHeader("content-disposition", "attachment;filename=" + exportFileName + ".xls");
+            ServletOutputStream os = response.getOutputStream();
+            Workbook workbook = new ExcelTransformer().transform(is, model);
+            workbook.write(os);
+        }
+    }
+    
+}
---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("")
+@RestController
+public class TestController {
+
+    @GetMapping("")
+    public ModelAndView test() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("year", 2022);
+
+        List<Object> list = new ArrayList<>();
+
+        User user1 = new User();
+        user1.setId("1");
+        user1.setName("张三");
+        list.add(user1);
+
+        User user2 = new User();
+        user2.setId("2");
+        user2.setName("李四");
+        list.add(user2);
+
+        map.put("users", list);
+
+        return new ModelAndView(new JettView("template.xls", "output"), map);
+
+    }
+    
+}
